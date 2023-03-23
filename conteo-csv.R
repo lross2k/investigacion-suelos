24f0d2f86e5ff935f540d06eb6c2b04c61c048d2
lross2k <luisross2000@outlook.com>
2023-03-23 06:02:03 -0600

    Muchas mejoras a conteo

--- conteo-csv.R
+++ conteo-csv.R
@@ -1,18 +1,46 @@
 install.packages(tidyverse)
 library(tidyverse)
 
-data <- read.csv("PERIODO_1.csv", sep = ";")
-data <- cbind(TIMESTAMP = format(as.Date(data$TIMESTAMP, "%d/%m/%Y %H:%M"), format="%d/%m/%Y"), data)
-data <- data[,-2]
+# Lee datos de CSV
+DataOriginal <- read.csv("Datos Base/COMPLETO.csv", sep = ";")
 
-#Conteo por día
-conteoPordia <- data %>% 
+# Agrupar cantidad de instancias según día
+Data <- DataOriginal
+Data <- cbind(TIMESTAMP = format(as.Date(Data$TIMESTAMP, "%d/%m/%Y %H:%M"), format="%d/%m/%Y"), Data)
+Data <- Data[,-2]
+# Agrupar fecha diaria y agregar contador de instancias
+ByDay <- Data %>% 
   group_by(TIMESTAMP) %>% 
-summarise(cantidad = n())
-head(conteoPordia)
-view(conteoPordia)
+summarise(INSTANCES = n())
+# Guardar en CSV el resumen de instancias por fecha
+write.csv2(ByDay, "Datos Base/DAILY_COUNT.csv", row.names = FALSE)
+# Limpia datos temporales
+rm(Data)
+
+# Agrupar cantidad de instancias según mes
+Data <- DataOriginal
+Data <- cbind(TIMESTAMP = format(as.Date(Data$TIMESTAMP, "%d/%m/%Y %H:%M"), format="%m/%Y"), Data)
+Data <- Data[,-2]
+# Agrupar fecha mensual y agregar contador de instancias
+ByMonth <- Data %>% 
+  group_by(TIMESTAMP) %>% 
+  summarise(INSTANCES = n())
+# Guardar en CSV el resumen de instancias por fecha
+write.csv2(ByMonth, "Datos Base/MONTHLY_COUNT.csv", row.names = FALSE)
+# Limpia datos temporales
+rm(Data)
+
+# Agrupar cantidad de instancias según año
+Data <- DataOriginal
+Data <- cbind(TIMESTAMP = format(as.Date(Data$TIMESTAMP, "%d/%m/%Y %H:%M"), format="%Y"), Data)
+Data <- Data[,-2]
+# Agrupar fecha anual y agregar contador de instancias
+ByYear <- Data %>% 
+  group_by(TIMESTAMP) %>% 
+  summarise(INSTANCES = n())
+# Guardar en CSV el resumen de instancias por fecha
+write.csv2(ByYear, "Datos Base/YEARLY_COUNT.csv", row.names = FALSE)
+# Limpia datos temporales
+rm(Data)
 
 #ggplot2::ggplot(ggplot2::aes(x = TIMESTAMP, y = SoilTemp_1), data = data) + ggplot2::geom_point() + ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90)) + ggplot2::theme(axis.text = ggplot2::element_text(size=10))
-
-library(readr)
-library(magrittr)
